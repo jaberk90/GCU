1f6d8f6d4c052c5fcbc07ea2d55362e149529e21
jaberk90 <jkaal@my.gcu.edu>
2026-03-30 23:38:52 -0700

    project dev for deploy

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { StockService } from './services/stock.service';
 
 @NgModule({
   declarations: [
@@ -17,12 +17,12 @@ import { StockService } from './services/stock.service';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
   ],
-  providers: [StockService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
